56d2dd151482afdf61443b63e0004877ab211bb3
headzoo <sean@headzoo.io>
2019-03-28 09:51:23 -0400

    Updates capture script

--- src/commands/capture.js
+++ src/commands/capture.js
@@ -22,33 +22,45 @@ page.open(url, function(status) {
     page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', function() {
         var sections = page.evaluate(function() {
             var sections  = [];
+            var variables = [];
 
             $('body').find('.block-section').each(function() {
                 var el = $(this);
-                sections.push({
-                    width:  el.width(),
-                    height: el.height(),
-                    left:   parseInt(el.offset().left, 10),
-                    top:    parseInt(el.offset().top, 10),
-                    html:   el.prop('outerHTML')
-                });
+
+                var variable = el.attr('data-style');
+                if (variable && variables.indexOf(variable) === -1) {
+                    variables.push(variable);
+                    sections.push({
+                        width:  el.width(),
+                        height: el.height(),
+                        left:   parseInt(el.offset().left, 10),
+                        top:    parseInt(el.offset().top, 10),
+                        html:   el.prop('outerHTML')
+                    });
+                }
             });
 
             return sections;
         });
 
         var components = page.evaluate(function() {
-            var components  = [];
+            var components = [];
+            var variables  = [];
 
             $('body').find('.block-component').each(function() {
                 var el = $(this);
-                components.push({
-                    width:  el.width(),
-                    height: el.height(),
-                    left:   parseInt(el.offset().left, 10),
-                    top:    parseInt(el.offset().top, 10),
-                    html:   el.prop('outerHTML')
-                });
+
+                var variable = el.attr('data-style');
+                if (variable && variables.indexOf(variable) === -1) {
+                    variables.push(variable);
+                    components.push({
+                        width:  el.width(),
+                        height: el.height(),
+                        left:   parseInt(el.offset().left, 10),
+                        top:    parseInt(el.offset().top, 10),
+                        html:   el.prop('outerHTML')
+                    });
+                }
             });
 
             return components;
